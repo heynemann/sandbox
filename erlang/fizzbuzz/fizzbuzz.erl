e9a09ebcbce58ee3681a93ff85acf139cb4c9154
Bernardo Heynemann <heynemann@gmail.com>
2010-06-07 21:45:16 -0500

    First commit

---
+++ erlang/fizzbuzz/fizzbuzz.erl
@@ -0,0 +1,18 @@
+-module(fizzbuzz).
+-export([start/0]).
+
+start() ->
+  calc(1,100).
+
+calc(Number,Max) when Number > Max -> noop;
+calc(Number,Max) ->
+  io:format("~s~n",[calc(Number)]),
+  calc(Number+1,Max).
+
+calc(Number) when
+                    Number rem 3 == 0,
+                    Number rem 5 == 0
+                -> "fizzbuzz";
+calc(Number) when Number rem 3 == 0 -> "fizz";
+calc(Number) when Number rem 5 == 0 -> "buzz";
+calc(Number) -> integer_to_list(Number).
