296e271b9694298bba0518570b9b7342f26ac11a
Bernardo Heynemann <heynemann@gmail.com>
2010-06-08 10:28:08 -0300

    Fizzbuzz mais simples ainda

--- erlang/fizzbuzz/fizzbuzz.erl
+++ erlang/fizzbuzz/fizzbuzz.erl
@@ -9,10 +9,7 @@ calc(Number,Max) ->
   io:format("~s~n",[calc(Number)]),
   calc(Number+1,Max).
 
-calc(Number) when
-                    Number rem 3 == 0,
-                    Number rem 5 == 0
-                -> "fizzbuzz";
+calc(Number) when Number rem 15 == 0 -> "fizzbuzz";
 calc(Number) when Number rem 3 == 0 -> "fizz";
 calc(Number) when Number rem 5 == 0 -> "buzz";
 calc(Number) -> integer_to_list(Number).
