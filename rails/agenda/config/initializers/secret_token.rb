b788ea0d419b6b6b535e0bb1f090cd3b4e5ad3ae
Bernardo Heynemann <heynemann@gmail.com>
2010-09-07 16:07:09 -0300

    Agenda e Vota_Prato

---
+++ rails/agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '6728cd81b452bc3fb5def497fd34a8f6a85075cdfbba8aad2bd720a19dab9654f2fca889573028830bbfad7a29a1454653e3cb246098b9ad96f9cbeb808d6c7a'
---
+++ rails/vota_prato/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = 'e506bfbc17d19f45896b36bbd6220e9a9c0c0fee3a033cfe1b79cd5df346f1313922a4acc7abac2b1cc7220db97dd1a59c6bdf90fec8c62b33e46eb5269324be'
