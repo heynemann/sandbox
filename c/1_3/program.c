1937a777987b80c97cf986558bea920cf2bcd370
Bernardo Heynemann <heynemann@gmail.com>
2013-04-14 19:32:14 -0300

    Program 3 done

---
+++ c/1_3/program.c
@@ -0,0 +1,42 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+
+void remove_duplicates(char* word) {
+    int i, j;
+    size_t length = strlen(word);
+
+    if (length == 1) {
+        return;
+    }
+
+    for(i=0; i < length; i++) {
+        if (word[i] == '-') {
+            word[i] = word[i + 1];
+        }
+
+        for (j=i+1; j < length; j++) {
+            if (word[i] == word[j]) {
+                word[j] = '-';
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    char word[255] = "test";
+    remove_duplicates(word);
+    printf("test = %s\n", word);
+
+    strcpy(word, "ee");
+    remove_duplicates(word);
+    printf("ee = %s\n", word);
+
+    strcpy(word, "a");
+    remove_duplicates(word);
+    printf("a = %s\n", word);
+
+    strcpy(word, "facebook");
+    remove_duplicates(word);
+    printf("facebook = %s\n", word);
+}
